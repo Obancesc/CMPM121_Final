42495f808c91eb92d363e780aad9ec16f9e61809
Unknown <obancesc@ucsc.edu>
2019-03-05 14:44:29 -0800

    First push of my work

---
+++ 121FinalAssignment/Assets/Scripts/Camera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player);
+    }
+}
---
+++ 121FinalAssignment/Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rigid;
+    public float forwardSpeed = 3.0F;
+    public float backwardSpeed = 2.0F;
+    public float rotateSpeed = 2.0F;
+    private Vector3 velo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        velo = new Vector3(0, 0, v);
+        velo = transform.TransformDirection(velo);
+        if(v > 0.1)
+        {
+            velo *= forwardSpeed;
+        } else if (v < -0.1) {
+            velo *= backwardSpeed;
+        }
+
+        transform.localPosition += velo * Time.fixedDeltaTime;
+       transform.Rotate(0, h * rotateSpeed, 0);
+
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            GetComponent<Animator>().Play("Swing_2_2");
+            if(Input.GetKeyDown(KeyCode.I))
+            {
+                GetComponent<Animator>().Play("Swing_2");
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            GetComponent<Animator>().Play("Swing_2_3");
+        }
+
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            GetComponent<Animator>().Play("Swing_3");
+        }
+    }
+}
